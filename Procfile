745f921efb29263ef23267c64335bd38e334a362
mdsami313 <samisanadi270@gmail.com>
2021-06-04 20:24:04 +0530

    Updated requirements file

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn dailytask.wsgi
--- taskmate/settings.py
+++ taskmate/settings.py
@@ -123,10 +123,14 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 LOGIN_REDIRECT_URL = "todolist"
 LOGIN_URL = "login"
+
+
 django_heroku.settings(locals())
+
